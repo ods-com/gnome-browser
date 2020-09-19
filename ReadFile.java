373ac98c6af6f5189337c5097923ab3a56794084
ods-com <71533448+ods-com@users.noreply.github.com>
2020-09-19 14:16:20 -0500

    Update and rename test.py to ReadFile.java

---
+++ ReadFile.java
@@ -0,0 +1,8 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.event.*;
+
+public class ReadFile extends JFrame{
+    
+}
--- test.py
+++
@@ -1,4 +0,0 @@
-import pygame
-import turtle
-
-class 76();
