e50b3d0354c04ba23de7d9215c608edb91f5e8b8
ods-com <71533448+ods-com@users.noreply.github.com>
2020-09-19 14:47:56 -0500

    Update ReadFile.java

--- ReadFile.java
+++ ReadFile.java
@@ -5,4 +5,34 @@ import javax.swing.event.*;
 
 public class ReadFile extends JFrame{
     
+    private JTextField addressBar;
+    private JEditorPane Display;
+    
+    //constructor
+    public ReadFile(){
+        super("omume browser")
+        
+        addressBar = new JTextField("please enter a URL.");
+        addressBar.addActionListener(
+            new ActionListener(){
+                Public void actionPerformed(ActionEvent event){
+                    loadpage(event.getActionCommand());
+                }
+
+            }
+        );
+        add(addressBar, BorderLayout.NORTH);
+        
+        display = new JEDITORPane();
+        display.setEditable(false);
+        display.addHyperlinkListener(
+            new Hyperlinklistener(){
+                public void hyperlinkupdate(HyperlinkEvent event)
+                    if(event.getType()==HyperlinkEvent.EventType.ACTIVATED){
+                        loadpage(event.getURL().toString());
+                    }
+            }
+        );
+    }
+
 }
