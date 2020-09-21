ad811c76474765946f2897af228bee97fe3e09c4
ods-com <71533448+ods-com@users.noreply.github.com>
2020-09-21 18:13:27 -0500

    Delete ReadFileMain.java

--- ReadFileMain.java
+++
@@ -1,8 +0,0 @@
-import javax.swing.JFrame;
-
-public class ReadFileMain {
-    public static void main(string[] args) {
-        ReadFile system = new ReadFile();
-        system.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    }
-}
